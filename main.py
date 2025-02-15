43639ed7280af11673a3a0074301b7e50c3ae0a8
Mayank Mehta <mehta26mayank@gmail.com>
2025-02-15 14:29:34 +0530

    hard coded mail

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def run_task(task: str = Query(...)):
     # Map the task_code to the corresponding internal function.
     try:
         if task_code == "A1":
-            user_email = os.environ.get("USER_EMAIL", "default@example.com")
+            user_email = "23f2001978@ds.study.iitm.ac.in"
             result = handle_task_A1(user_email)
         elif task_code == "A2":
             result = handle_task_A2()
